dedf7592c56a06cf58cbf6cc54048ce3fe442e8a
Kai Peng <67889629+P-kai@users.noreply.github.com>
2024-04-09 21:04:02 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,3 +11,26 @@
     # Optional: Initialize environment variables, which may improve the success rate of software installation
     PATH=${soft}/bin:${soft}/condabin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:${db}/EasyMicrobiome/linux:${db}/EasyMicrobiome/script
     echo $PATH
+
+# 下载最新版miniconda3
+    wget -c https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+    # 安装，-b批量，-f无提示，-p目录，许可协议打yes
+    bash Miniconda3-latest-Linux-x86_64.sh -b -f -p ${soft}
+    # 激活，然后关闭终端重开，提示符前出现(base)即成功
+    ${soft}/condabin/conda init
+    source ~/.bashrc
+    # 查看版本，conda 23.7.3, python 3.11.4
+    conda -V 
+    python --version
+    # 添加常用频道
+    conda config --add channels bioconda # 生物软件
+    conda config --add channels conda-forge # Highest priority
+
+    # conda默认配置文件为 ~/.condarc 查看配置文件位置
+    conda install mamba -c conda-forge -y
+    mamba install pandas -c conda-forge -y
+    mamba install conda-pack -c conda-forge -y
+    conda config --set channel_priority strict
+    conda config --show-sources
+    # 查看虚拟环境列表 
+    conda env list
