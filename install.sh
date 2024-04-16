caf2e4ec47d1898820d7d81092a8c07e2668d397
Kai Peng <67889629+P-kai@users.noreply.github.com>
2024-04-16 13:27:37 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,63 +1,59 @@
-# 本脚本用于EasyNanoMeta分析流程的软件安装
-## 设置数据库路径及软件安装路径
+# The script for tools installation of EasyNanoMeta analysis pipeline (本脚本用于EasyNanoMeta分析流程的软件安装)
+## Set the database path and software installation path (设置数据库路径及软件安装路径)
 
+    # Database location (数据库位置)
     db=~/db
     mkdir -p ${db} && cd ${db}
-    # 软件安装位置Software installation location，默认为~/miniconda3，测试服务器为/anaconda3
+    # Software installation location. Default ~/miniconda3 (软件安装位置, 默认为~/miniconda3）
     soft=~/miniconda3
-    # 其它软件安装位置Other softwares installation location，默认为~/tools
+    # Other softwares installation location. Default ~/tools（其它软件安装位置，默认为~/tools）
     tool=~/tools
-    # 经常使用的服务器环境，可把全文${db}和${soft}替换为绝对路径，将不再需要每次读取以上环境变量
-    # In the frequently used server environment, you can replace the variable ${db} and ${soft} with absolute paths, and you will no longer need to run the above environment variables every time
-    # 可选：初始化环境变量，可能提高软件安装成功率
-    # Optional: Initialize environment variables, which may improve the success rate of software installation
-    PATH=${soft}/bin:${soft}/condabin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:${db}/EasyMicrobiome/linux:${db}/EasyMicrobiome/script
-    echo $PATH
+    # In the frequently used server environment, you can replace the variable ${db} and ${soft} with absolute paths, and you will no longer need to run the above environment variables every time.
+    # 经常使用的服务器环境，可把全文${db}和${soft}替换为绝对路径，将不再需要每次读取以上环境变量 
 
-## 下载最新版miniconda3，软件管理工具
+
+##  Download the latest version of the software management tool miniconda3 (下载最新版miniconda3，软件管理工具)
     wget -c https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-    # 安装，-b批量，-f无提示，-p目录，许可协议打yes
+    # Install, -b batch, -f no prompt, -p directory, license agreement hit yes (安装，-b批量，-f无提示，-p目录，许可协议打yes)
     bash Miniconda3-latest-Linux-x86_64.sh -b -f -p ${soft}
-    # 激活，然后关闭终端重开，提示符前出现(base)即成功
+    # Activate, then close the terminal to reopen, the prompt appears before (base) is successful 激活，然后关闭终端重开，提示符前出现(base)即成功
     ${soft}/condabin/conda init
     source ~/.bashrc
-    # 查看版本，conda 23.7.3, python 3.11.4
+    # Check the versions, conda 23.7.3, python 3.11.4 (查看版本，conda 23.7.3, python 3.11.4)
     conda -V 
     python --version
-    # 添加常用频道
-    conda config --add channels bioconda # 生物软件
-    conda config --add channels conda-forge # Highest priority
+    # Add frequent channels (添加常用频道)
+    conda config --add channels bioconda
+    conda config --add channels conda-forge
 
-    # conda默认配置文件为 ~/.condarc 查看配置文件位置
+    # The default conda config is ~/.condarc. (conda默认配置文件为 ~/.condarc 查看配置文件位置)
     conda install mamba -c conda-forge -y
     mamba install pandas -c conda-forge -y
     mamba install conda-pack -c conda-forge -y
     conda config --set channel_priority strict
     conda config --show-sources
-    # 查看虚拟环境列表 
+    # Check the environment list (查看虚拟环境列表)
     conda env list
 
-# 1.安装数据质控及去宿主软件
-## 1.1 长读数据质控软件
-### 安装porechop_abi
-    # 创建porechop_abi单独的conda环境，使用conda安装软件
+# 1. Install tools for data quality control and host DNA removal (安装数据质控及去宿主软件)
+## 1.1 Quality control tools for nanopore long read data (长读数据质控软件)
+### porechop_abi installation (安装porechop_abi)
+    # Create a separate conda environment for porechop_abi and install the software using conda (创建porechop_abi单独的conda环境，使用conda安装软件)
     conda create -y -n porechop_abi
     conda activate porechop_abi
     conda install -f -c conda-forge -c bioconda  porechop_abi
 
-### 安装nanopack
-    #创建nanopack环境,python3.10版本可成功安装，其他python版本可能安装失败。
+### nanopack installation (安装nanopack)
+    # Create a separate conda environment with python=3.10 (创建nanopack环境,python3.10版本可成功安装，其他python版本可能安装失败)
     conda create -y -n nanopack python=3.10 
     conda activate nanopack
     
-    #使用pip安装nanopack
+    # Install nanopack using pip in conda nanopack environment (在conda的nanopack环境中，使用pip安装nanopack)
     pip install nanopack
 
-    #不成功，可尝试使用pip的国内清华源安装nanopack，由于网络问题的失败，可多尝试几次。
+    # If the installation is not successful, you can try to install nanopack using pip's domestic Tsinghua source, and you can try several more times due to the failure due to network issues.
+    # 如果安装不成功，可尝试使用pip的国内清华源安装nanopack，由于网络问题的失败，可多尝试几次。
     pip install -i https://pypi.tuna.tsinghua.edu.cn/simple nanopack 
-
-    #注意最终安装成功的软件路径提示，一般为path下
-    /ifs1/User/yongxin/.local/bin/
 
 ## 1.2 长读数据去宿主软件
 ### 安装minimap2，samtools，bedtools
